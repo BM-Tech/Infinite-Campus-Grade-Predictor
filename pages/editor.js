532acb1ea860f5366619099555dd2e97112f238d
Benjamin Man <benjamin.man1001@gmail.com>
2021-11-21 23:50:32 -0800

    delete old html

--- pages/editor.js
+++
@@ -1,5 +0,0 @@
-chrome.storage.local.get(["data"], (res) => {
-    console.log(res)
-    document.getElementById("className").innerText = res.data.className
-    document.getElementById("grade").innerText = res.data.grade + " (" + res.data.score + "%)"
-})
